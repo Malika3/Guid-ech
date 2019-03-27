2be14271f89b3742dd7cf7d7965782c1cbdb86ba
Ikhlasszh <48415305+Ikhlasszh@users.noreply.github.com>
2019-03-27 18:47:43 +0100

    Add files via upload

---
+++ app/Http/Controllers/controlBoutiqueRegister.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class controlBoutiqueRegister extends Controller
+{
+    //
+     public function index()
+    {
+        return view('formulaires.registerBoutique');
+    }
+}
